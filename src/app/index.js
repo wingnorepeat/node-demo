ce12ceb3bd2f06dc6ae265ad352410b9db241e5d
LAPTOP-43LHSDBV\73005 <730051080@qq.com>
2023-11-03 18:11:04 +0800

    init

---
+++ src/app/index.js
@@ -0,0 +1,11 @@
+const Koa = require("koa");
+const bodyParser = require("koa-bodyparser");
+const userRouter = require("../router/user.router");
+
+const app = new Koa();
+
+app.use(bodyParser());
+app.use(userRouter.routes());
+app.use(userRouter.allowedMethods());
+
+module.exports = app;
---
+++ src/config/error.js
@@ -0,0 +1,7 @@
+const NAME_IS_ALREADY_EXISTS = "name_is_already_exists";
+const NAME_OR_PASSWORD_IS_REQUIRED = "name_or_password_is_required";
+
+module.exports = {
+  NAME_IS_ALREADY_EXISTS,
+  NAME_OR_PASSWORD_IS_REQUIRED
+};
